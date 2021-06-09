a48abc58fd58aa87c8e827e93485eac65f36cef9
Gabriel Lemache <lemache35@gmail.com>
2021-06-09 10:25:58 -0300

    add pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import Link from 'next/link'
+
 function home(){
-    return <div>hello</div>
+    return(
+        <h1 className="title">
+            Read{' '}
+            <Link href="/posts/first-post">
+            <a>this page!</a>
+            </Link>
+        </h1>
+    )
 }
 
 export default home
