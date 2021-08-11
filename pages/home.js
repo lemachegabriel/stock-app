831405d33f4b0e63c406680f610fed1d8afffbb6
Gabriel Lemache <lemache35@gmail.com>
2021-08-10 21:52:40 -0300

    testes

---
+++ pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Home extends React.Component{
+    componentDidMount(){
+        getUserProfile()
+    }
+
+    render(){
+        return(
+            <div>profile</div>
+        )
+    }
+}
